b9eb4a4797fb8465a6416ac2201b33c3a4306445
Sudhakar <usudhakar@informatica.com>
2021-02-19 20:22:59 +0530

    updated end to end time loop counter to 0

--- utils/endToEnd.go
+++ utils/endToEnd.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"strconv"
 	"time"
 
@@ -18,20 +17,25 @@ func EndToEndTime(jobData config.JobResponse) string {
 
 	//fmt.Println(jobData)
 
-	fmt.Println(jobData[0].Type)
-	//smaxTime := jobData[0].EndTime
+	//fmt.Println(jobData[0].Type)
+	//fmt.Println(jobData[0].EndTime)
 	//sminTime := jobData[0].StartTime
 	smaxTime := "1513561392896"
 	sminTime := "1713561392896"
 
 	maxTime, _ := strconv.ParseInt(smaxTime, 10, 64)
 	minTime, _ := strconv.ParseInt(sminTime, 10, 64)
-	for i := 1; i < len(jobData); i++ {
+	//fmt.Println(len(jobData))
+	for i := 0; i < len(jobData); i++ {
 
 		if (jobData[i].Type != "Purge") && (jobData[i].Type != "Delete") && (jobData[i].Type != "Configuration purge") {
-			//fmt.Println(jobData[i].Type)
+			//	fmt.Println(jobData[i].Type)
+			//	fmt.Println(jobData[i].EndTime)
+			//	fmt.Println(maxTime)
+			//jobData[i].EndTime
 			if IntVal(jobData[i].EndTime) > maxTime {
 				maxTime = IntVal(jobData[i].EndTime)
+				//	fmt.Println("hi")
 
 			}
 
@@ -45,8 +49,8 @@ func EndToEndTime(jobData config.JobResponse) string {
 	startTime := time.Unix(0, minTime*int64(time.Millisecond))
 	endTime := time.Unix(0, maxTime*int64(time.Millisecond))
 
-	fmt.Println(endTime)
-	fmt.Println(startTime)
+	//fmt.Println(endTime)
+	//fmt.Println(startTime)
 	diff := endTime.Sub(startTime)
 	//fmt.Printf("%f", diff.Seconds())
 	//p := fmt.Sprintf("%02d:%02d:%02d", int64(diff.Hours()), int64(diff.Minutes()), int64(diff.Seconds()))
